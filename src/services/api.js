c9c9a7ebbace74d25d78636284cb31239ffb7af2
Musaddik Hossain <musaddikh13email.com>
2025-12-04 13:04:12 +0600

    fix base url 2

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://colour-matching-ai-sarver-wb7d.onrender.com";
 
 console.log("API Base URL:", API_BASE_URL);
 
